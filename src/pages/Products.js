66d2fe24ae4377b54b419ced613bf7cdc2b8dbd1
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-09-11 22:13:31 +0530

    css refactor

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -19,11 +19,11 @@ export const Products = () => {
         <p className='product-para' > Manage your products and view their sales performance </p>
 
         <div className='product-nav' >
-            <p className='headingName' style={{marginLeft: "6rem"}} >Name</p>
-            <p className='headingStatus' style={{marginLeft: "17rem"}} >Status</p>
-            <p className='headingPrice' style={{marginLeft: "6rem"}} >Price</p>
-            <p className='headingSales' style={{marginLeft: "5rem"}} >Total Sales</p>
-            <p className='headingCreated' style={{marginLeft: "3rem"}} >Created at</p>
+            <p className='headingName' >Name</p>
+            <p className='headingStatus' >Status</p>
+            <p className='headingPrice' >Price</p>
+            <p className='headingSales' >Total Sales</p>
+            <p className='headingCreated' >Created at</p>
         </div>
         { Data.data.length > 1 &&
             Data.data.slice(page * 5 - 5, page * 5).map((item) => 
@@ -35,14 +35,14 @@ export const Products = () => {
                     <p className='product-sales'> {item.totalSales} </p>
                     <p className='product-create'> {item.createdAt} </p>
                     <p className='product-action' onClick={() => handleClick(item._id)} > <HiOutlineDotsHorizontal /> </p>
-                    { show === item._id ?
+                    { show === item._id &&
                        <div className='actions'>
                        <p style={{fontWeight: "500"}} >Actions</p>
                        <p>Edit</p>
                        <p>Delete</p>
-                       </div> : ""
+                       </div>
                     }   
-                    </div>
+                </div>
         )}
         <div className='product-footer'>
          <p className='footer-para'>Showing <span className='span'>{page * 5 - 4}-{page * 5}</span> of <span className='span'>{Data.data.length}</span> products </p>
